c4a2d4fa59878bac4304175f7a4a7593c05f65e4
Gabriel Cornejo <gcornejo441@gmail.com>
2025-01-13 06:29:52 -0500

    feat: endpoint and query testing.

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI, Query
 import chromadb
 from sentence_transformers import SentenceTransformer
-
 from utils.retriever import retrieve_relevant_docs
 
 app = FastAPI()
 
-client = chromadb.Client()
-collection = client.create_collection(name="domain_docs")
+client = chromadb.PersistentClient(path="./db/chroma_storage")
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+collection = client.get_or_create_collection(name="domain_docs")
 
+model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
 @app.get("/query")
 def query_consume(user_query: str = Query(...)):
+    """
+    Endpoint to query documents based on user input.
+    """
+    print(f"Received query: {user_query}")
     docs = retrieve_relevant_docs(user_query, model, collection)
     return {"query": user_query, "retrieved_docs": docs}
