268408bab0f3776213e164d36c5e355c4d183b25
Gabriel Cornejo <gcornejo441@gmail.com>
2025-10-04 21:58:07 -0400

    feat: enhance _file2dict function to improve file handling and validation

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,23 @@
 import os.path as osp
+import tempfile
+# https://github.com/modelscope/modelscope/blob/master/modelscope/utils/config.py
+
 
 def _file2dict(filename):
-    filename = osp.abspath(osp.expanduser(filename))
-    if not osp.exists(filename):
-        raise FileNotFoundError(f"File does not exist: {filename}")
-    fileExtname = osp.splitext(filename)[1]
+    # points to file
+    filename = osp.abspath(filename)
 
-    print(f"fileExtname: {fileExtname}")
+    #  checks if file exists
+    if not osp.exists(filename):
+        raise FileNotFoundError(f'File {filename} does not exist.')
+
+    fileExtName = osp.splitext(filename)[1]
+    if not fileExtName in ['.yaml', '.yml']:
+        raise ValueError('Only yaml or yml file is supported.')
+
+    with tempfile.TemporaryDirectory() as tmp_cfg_dir:
+
+        tmp_cfg_file = tempfile.NamedTemporaryFile(
+            dir=tmp_cfg_dir,
+            suffix=fileExtName
+        )
